7a23559d6167a7b7bdac6cabe14fb9d69c5d1577
Choi Hong Gyu <zxolqs@gmail.com>
2023-07-03 12:58:02 +0900

    update icons

---
+++ src/components/IconCancel.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const IconCancel = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 22 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1.80711 22L0 20.1929L9.19289 11L0 1.80711L1.80711 0L11 9.19289L20.1929 0L22 1.80711L12.8071 11L22 20.1929L20.1929 22L11 12.8071L1.80711 22Z"
+      fill="black"
+    />
+  </svg>
+);
+export default IconCancel;
---
+++ src/components/IconCaretDown.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const IconCaretDown = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 13 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M6.5 7.58399L0 0H13L6.5 7.58399Z" fill="black" />
+  </svg>
+);
+export default IconCaretDown;
