924547bed49c65c7f5d608e73b2e4521bafed192
Choi Hong Gyu <zxolqs@gmail.com>
2023-08-17 13:07:37 +0900

    update mascot and check

---
+++ src/components/IconMascot1.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const IconMascot1 = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 67 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M67 32.5152L39.1869 26.8269L33.4986 -1L27.8131 26.8269L0 32.5152L27.8131 38.2062H27.8186L33.4986 66L39.1814 38.2062H39.1869L67 32.5152Z"
+      fill="#AFF73C"
+    />
+  </svg>
+);
+export default IconMascot1;
